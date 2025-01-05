e3380d200b53475d50834f35e39f684534edec75
Weronika Gozdera <weronika.gozdera@gmail.com>
2025-01-05 17:06:45 +0100

    Added cors middleware for front app (local)

--- query-handler.go
+++ query-handler.go
@@ -110,3 +110,19 @@ func (h *QueryHandler) handleQuery(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(result)
 }
+
+func (h *QueryHandler) corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3006")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
